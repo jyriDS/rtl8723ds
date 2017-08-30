9c39904b742daf93c83c6ebbce395351aad48ef3
jyriDS <31311452+jyriDS@users.noreply.github.com>
2017-08-30 08:10:26 +0300

    trying this again
    maybe the if will work

--- include/osdep_service_linux.h
+++ include/osdep_service_linux.h
@@ -45,7 +45,11 @@
 	#include <linux/semaphore.h>
 #endif
 #include <linux/sem.h>
-#include <linux/sched/signal.h>
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0))
+	#include <linux/sched/signal.h>
+#else
+	#include <linux/sched.h>
+#endif
 #include <linux/etherdevice.h>
 #include <linux/wireless.h>
 #include <net/iw_handler.h>
